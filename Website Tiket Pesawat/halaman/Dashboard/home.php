6c330ff08eeebf76b71300fb42c0682b626a8c34
AnandaRezaKurniawan <72422050+AnandaRezaKurniawan@users.noreply.github.com>
2021-08-01 22:25:37 +0700

    Add files via upload

---
+++ Website Tiket Pesawat/halaman/Dashboard/home.php
@@ -0,0 +1,13 @@
+<center>
+<h1 style="font-family: CentSchbkCyrill BT; text-decoration: underline;">Selamat Datang Di</h1>
+<h1 style="font-family: CentSchbkCyrill BT; text-align: center; text-decoration: underline;">Aplikasi Tiket Pesawat Terbang dan Kereta Api</h1>
+<br><br>
+	<div class='panel panel-default' style="border-radius: 15px;">
+			<p>
+				<img src='image/asdf.png'>
+			</p>
+
+	</div>	
+	<br>
+	<p>@Copyrigt Ananda Reza Kurniawan, Dilarang Keras MengCopy!!!</p>
+<br><br>
